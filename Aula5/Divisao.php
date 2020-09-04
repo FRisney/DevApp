56939c20446eca3d781c810613ddd509e7a44bff
FRisney <frisney@protonmail.com>
2020-09-03 23:19:54 -0300

    Tratamento de divisão por zero

--- Aula5/Divisao.php
+++ Aula5/Divisao.php
@@ -3,8 +3,12 @@
 class Divisao extends Calculo{
     function __construct(float $num1, float $num2){
         $this->operador1 = $num1 ?? 0;
-        $this->operador2 = $num2 ?? 0;
-        $this->calcular();
+        if ($num2 == 0) {
+            $this->resultado = 'Divisao por zero';
+        }else {
+            $this->operador2 = $num2 ?? 0;
+            $this->calcular();
+        }
     }
     public function calcular(){
         $this->resultado = $this->operador1 / $this->operador2;
